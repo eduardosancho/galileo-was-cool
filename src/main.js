72daedeffc731b01e1c2fe164d2b97959c3b6ef4
Eduardo Sancho <hello@eduardosancho.com>
2025-09-13 14:19:38 -0600

    Fix: Adjust aspect on resize

--- src/main.js
+++ src/main.js
@@ -17,28 +17,42 @@ const camera = new THREE.PerspectiveCamera(
   window.innerWidth / window.innerHeight,
   0.1,
   200
-)
+);
+
+// const aspectRatio = window.innerWidth / window.innerHeight;
+
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   200
+// );
+
 camera.position.z = 5;
 
 const canvas = document.querySelector('canvas.threejs');
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 });
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.autoRotate = true;
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+})
 
-const renderLoop= () => {
+const renderLoop = () => {
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderLoop);
 };
 
 renderLoop();
-
-
-
 
