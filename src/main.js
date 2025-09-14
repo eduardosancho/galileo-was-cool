952fb3df6e00530e07274f0634ec6bb018b2f7d1
Eduardo Sancho <hello@eduardosancho.com>
2025-09-14 17:51:35 -0600

    Feat: Groups

--- src/main.js
+++ src/main.js
@@ -9,27 +9,32 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const torusKnotGeometry = new THREE.TorusKnotGeometry(0.5, 0.15, 100, 16);
 const planeGeometry = new THREE.PlaneGeometry(1, 1);
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
 
-const material = new THREE.MeshPhysicalMaterial();
-material.color = new THREE.Color('green');
+const material = new THREE.MeshBasicMaterial();
 
-pane.addBinding(material, 'metalness', { min: 0, max: 1, step: 0.01 });
-pane.addBinding(material, 'roughness', { min: 0, max: 1, step: 0.01 });
-// reflectivity
-pane.addBinding(material, 'reflectivity', { min: 0, max: 1, step: 0.01 });
-// clearcoat
-pane.addBinding(material, 'clearcoat', { min: 0, max: 1, step: 0.01 });
+const group = new THREE.Group();
 
-const mesh = new THREE.Mesh(geometry, material);
-const mesh2 = new THREE.Mesh(torusKnotGeometry, material);
-mesh2.position.x = 1.5;
+const cube = new THREE.Mesh(geometry, material);
+const knot = new THREE.Mesh(torusKnotGeometry, material);
+knot.position.x = 1.5;
 
 const plane = new THREE.Mesh(planeGeometry, material);
 plane.position.x = -1.5;
 
-scene.add(mesh);
-scene.add(mesh2);
-scene.add(plane);
+const sphere = new THREE.Mesh();
+sphere.geometry = sphereGeometry;
+sphere.material = material;
+sphere.position.y = 1.5;
+
+const cylinder = new THREE.Mesh();
+cylinder.geometry = cylinderGeometry;
+cylinder.material = material;
+cylinder.position.y = -1.5;
+
+group.add(cube, knot, plane, sphere, cylinder);
+scene.add(group);
 
 const light = new THREE.AmbientLight('white', 0.4);
 scene.add(light);
@@ -67,6 +72,11 @@ window.addEventListener('resize', () => {
 });
 
 const renderLoop = () => {
+  group.children.forEach(child => {
+    if (child instanceof THREE.Mesh) {
+      child.rotation.y += 0.01;
+    }
+  });
 
   controls.update();
   renderer.render(scene, camera);
